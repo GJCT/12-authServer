c613c62766181caaf9b60ae13fdeef4b4060cf63
kidCode <gabriel.cardona3@soy.sena.edu.co>
2023-01-12 09:42:59 -0500

    Correcciones

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const { dbConnection } = require('./db/config');
+const { response } = require('express');
 require('dotenv').config();
 
 //Crear el servidor/App de express
@@ -21,6 +24,10 @@ app.use(express.json());
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 
+//Manejar rutas
+// app.get('*', (req, resp = response)=>{
+//     resp.sendFile(path.resolve(__dirname, 'public/index.html'))
+// })
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
